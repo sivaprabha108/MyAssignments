c4770c363ed329648d5a5a0f0d8b86ca252ae10c
sivaprabha108 <110895847+sivaprabha108@users.noreply.github.com>
2022-08-13 00:46:50 +0530

    Add files via upload
    Completed Week1 Day1 Assignment.

---
+++ day1/ConvertANegativeNumberToPositiveNumber.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class ConvertANegativeNumberToPositiveNumber {
+
+	public static void main(String[] args) {
+		int number  = -10;
+		if (number<0)
+		{
+			number = -number;
+			System.out.println("The Positive number is: " +number);
+		}
+
+	}
+
+}
